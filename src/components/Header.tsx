cb48b69f347dfbdfc7158d0359475da28c1f27df
dev-ardha <ardhaydhtm@gmail.com>
2020-07-16 11:59:55 +0700

    Initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header(){
+    return(
+        <header className="header">
+            <h1>Just Say The Magic Words!</h1>
+            <input type="text" placeholder="a red button with rounded corner"/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Navbar(){
+    return(
+        <nav className="navbar">
+            <div className="navbar-title">
+                <h1>CodeWizard</h1>
+            </div>
+            <div className="navbar-items">
+                <a href="/">Home</a>
+                <a href="/cheatsheet">Cheatsheet</a>
+                <a href="/about">About</a>
+                <a href="https://github.com/dev-ardha/codewizard">GitHub</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
